0b12e8327d56a903626d9e91237cdfb407ef3337
yeeeargh <sebatian@hauser.tv>
2013-07-25 14:20:46 +0200

    moved my repo

--- test-script.bat
+++ test-script.bat
@@ -1,29 +1,29 @@
 @ECHO OFF
 
-set aadpath=.\AlbumArtDownloaderXUI-0.46
+set aadpath=..\AlbumArtDownloaderXUI-0.46
 set aadbin=%aadpath%\AlbumArt.exe
 set aadscripts=%aadpath%\Scripts
 
 :: Musicbrainz
-cp -u musicbrainz.boo %aadscripts%
+copy musicbrainz.boo %aadscripts%
 
 :: Example from Musicbrainz
-start %aadbin% /artist "Portishead" /album "Dummy" /sources "Musicbrainz"
+::start %aadbin% /artist "Portishead" /album "Dummy" /sources "Musicbrainz"
 
 :: Album which contains &. This album couldn't be found when characters like this got stripped
-start %aadbin% /artist "Volbeat" /album "Outlaw gentlemen & shady ladies" /sources "Musicbrainz"
+::start %aadbin% /artist "Volbeat" /album "Outlaw gentlemen & shady ladies" /sources "Musicbrainz"
 
 :: Had some unusual results while searching for this album
-start %aadbin% /artist "The Real McKenzies" /album "10,000 shots" /sources "Musicbrainz"
+::start %aadbin% /artist "The Real McKenzies" /album "10,000 shots" /sources "Musicbrainz"
 
 :: Just an album name
-start %aadbin% /album "Dummy" /sources "Musicbrainz"
+::start %aadbin% /album "Dummy" /sources "Musicbrainz"
 
 :: 7digital Webservice
-cp -u 7digital-ws.boo %aadscripts%
+copy 7digital-ws.boo %aadscripts%
 
 :: Example
-start %aadbin% /artist "Portishead" /album "Dummy" /sources "7digital-ws"
+::start %aadbin% /artist "Portishead" /album "Dummy" /sources "7digital-ws"
 
 :: Album with special characters
 start %aadbin% /artist "Volbeat" /album "Outlaw gentlemen & shady ladies" /sources "7digital-ws"
