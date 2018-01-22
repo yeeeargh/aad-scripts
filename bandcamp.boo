7e41da31099d6caf6fe302b2ddddb31ea957eadb
Sebastian Hauser <sebastian@hauser.tv>
2018-01-22 14:28:17 +0100

    added bandcamp script to repo

---
+++ bandcamp.boo
@@ -0,0 +1,43 @@
+import System
+import System.Text.RegularExpressions
+import AlbumArtDownloader.Scripts
+import util
+
+class Bandcamp(AlbumArtDownloader.Scripts.IScript, ICategorised):
+
+	Name as string:
+		get: return "bandcamp"
+	Version as string:
+		get: return "0.3"
+	Author as string:
+		get: return "Alex Vallat"
+	Category as string:
+		get: return "Independent"
+	def Search(artist as string, album as string, results as IScriptResults):
+		artist = StripCharacters("\";:?!", artist)
+		album = StripCharacters("\";:?!", album)
+		query = ""
+
+		if (artist.Length!=0):
+			query += EncodeUrl("\"${artist}\" ")
+		if (album.Length!=0):
+			query += EncodeUrl("\"${album}\"")
+
+		//Retrieve the search results
+		searchResultsHtml as string = GetPage("http://bandcamp.com/search?q=${query}")
+
+		matches = Regex("<a class=\"artcont\" href=\"(?<url>[^\"]+)\">\\s*<div class=\"art\">\\s*<img src=\"https?:(?<img>[^\"]+?)_7\\.jpg\".*?<div class=\"heading\">\\s*<a[^>]+>\\s*(?<title>.+?)\\s*<", RegexOptions.Singleline | RegexOptions.IgnoreCase).Matches(searchResultsHtml)
+		
+		results.EstimatedCount = matches.Count
+		
+		for match as Match in matches:
+			url = match.Groups["url"].Value;
+			title = match.Groups["title"].Value;
+			img = match.Groups["img"].Value;
+			thumb = "http:" + img + "_7.jpg";
+			full = "http:" + img + "_10.jgp";
+			
+			results.Add(thumb, title, url, -1, -1, full, CoverType.Front)
+
+	def RetrieveFullSizeImage(url):
+		return url
--- test-script.bat
+++ test-script.bat
@@ -1,44 +1,43 @@
 @ECHO OFF
 
-set aadpath=..\AlbumArtDownloaderXUI-0.46
+set aadpath=..\AlbumArtDownloaderXUI-1.02
 set aadbin=%aadpath%\AlbumArt.exe
 set aadscripts=%aadpath%\Scripts
 
 :: Musicbrainz
-copy musicbrainz.boo %aadscripts%
-
+::copy musicbrainz.boo %aadscripts%
 :: Example from Musicbrainz
-start %aadbin% /artist "Portishead" /album "Dummy" /sources "Musicbrainz"
-
+::start %aadbin% /artist "Portishead" /album "Dummy" /sources "Musicbrainz"
 :: Album which contains &. This album couldn't be found when characters like this got stripped
-start %aadbin% /artist "Volbeat" /album "Outlaw gentlemen & shady ladies" /sources "Musicbrainz"
-
+::start %aadbin% /artist "Volbeat" /album "Outlaw gentlemen & shady ladies" /sources "Musicbrainz"
 :: Had some unusual results while searching for this album
-start %aadbin% /artist "The Real McKenzies" /album "10,000 shots" /sources "Musicbrainz"
-
+::start %aadbin% /artist "The Real McKenzies" /album "10,000 shots" /sources "Musicbrainz"
 :: Multiple artists
-start %aadbin% /artist "Leatherface & Hot Water Music" /album "BYO Split Series, Volume I" /sources "Musicbrainz"
-
+::start %aadbin% /artist "Leatherface & Hot Water Music" /album "BYO Split Series, Volume I" /sources "Musicbrainz"
 :: Just an album name
-start %aadbin% /album "Dummy" /sources "Musicbrainz"
-
+::start %aadbin% /album "Dummy" /sources "Musicbrainz"
 :: Encoding of  ! # $ % & ' ( ) * + , / : ; = ? @ [ ] for Lucene
-start %aadbin% /artist "Oasis" /album "(What's the Story) Morning Glory?" /sources "Musicbrainz"
-
+::start %aadbin% /artist "Oasis" /album "(What's the Story) Morning Glory?" /sources "Musicbrainz"
 :: Less strict search with lucene groupings
-start %aadbin% /artist "xploschns in se sky" /album "how strance, inno" /sources "Musicbrainz"
-
+::start %aadbin% /artist "xploschns in se sky" /album "how strance, inno" /sources "Musicbrainz"
 :: Other cover art types
-start %aadbin% /artist "Matula" /album "Kolumbus EP" /sources "Musicbrainz"
-start %aadbin% /artist "Die Žrzte" /album "Le Frisur" /sources "Musicbrainz"
+::start %aadbin% /artist "Matula" /album "Kolumbus EP" /sources "Musicbrainz"
+::start %aadbin% /artist "Die Žrzte" /album "Le Frisur" /sources "Musicbrainz"
 
 
 :: 7digital Webservice
-copy 7digital-ws.boo %aadscripts%
-
+::copy 7digital-ws.boo %aadscripts%
 :: Example
-start %aadbin% /artist "Portishead" /album "Dummy" /sources "7digital-ws"
-
+::start %aadbin% /artist "Portishead" /album "Dummy" /sources "7digital-ws"
 :: Album with special characters
-start %aadbin% /artist "Volbeat" /album "Outlaw gentlemen & shady ladies" /sources "7digital-ws"
+::start %aadbin% /artist "Volbeat" /album "Outlaw gentlemen & shady ladies" /sources "7digital-ws"
 
+:: Discogs
+::copy discogs.boo %aadscripts%
+:: Example
+::start %aadbin% /artist "Beatsteaks" /album "Beatsteaks" /sources "Discogs"
+
+:: Bandcamp
+copy bandcamp.boo %aadscripts%
+:: Example
+start %aadbin% /artist "Big Walnuts Yonder" /album "Big Walnuts Yonder" /sources "Bandcamp"
